9d34e850e53976d0da5ad9dc0fbc8e79e7b6f09e
mishaqwerly <mishaqwerlt@gamil.com>
2020-12-24 11:12:40 +0200

    add routes for posts

--- api/server.js
+++ api/server.js
@@ -2,15 +2,18 @@ const express = require('express')
 require("dotenv").config();
 const port = process.env.PORT;
 const app = express()
-
-app.use(function(request, response, next){
-    console.log("Middleware 1");
-    next();
-});
+const posts = require('./routes/posts');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/posts', posts);
+
+app.use(function(request, response, next){
+  console.log("Middleware 1");
+  next();
+});
 
 app.use((req, res) => {
   res.status(404).send('404');
