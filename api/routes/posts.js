a40df70b1560236b997a5ca5e553e8fadee46c7d
mishaqwerly <mishaqwerlt@gamil.com>
2021-02-22 17:58:57 +0200

    remove console log

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,13 +5,12 @@ const passport = require('passport');
 const {checkAuthorized} = require("../middleware/acl");
 const authenticate = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, async (err, user) => {
-    console.log(user)
     req.user = user;
     next();
   })(req, res, next);
 };
 
-router.get("/",[authenticate, checkAuthorized('posts', 'user_id')], postController.getAllPosts);
+router.get("/",[authenticate], postController.getAllPosts);
 // router.get("/", postController.getAllPosts);
 router.get('/:id', postController.getPostsById);
 router.post('/',[authenticate], postController.createNewPost)
