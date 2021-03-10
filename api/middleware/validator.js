2539432b73f6ea357faace644c111a3ef514cc2f
mishaqwerly <mishaqwerlt@gamil.com>
2021-03-10 12:04:09 +0200

    add validators

---
+++ api/middleware/validator.js
@@ -0,0 +1,53 @@
+const db = require('../services/db')
+module.exports = function validator(validatorSchema) {
+  return async (req, res, next) => {
+    const errors = []
+    Object.entries(validatorSchema).forEach(([fieldName, rules]) => {
+      rules.forEach((rule) => {
+        const [ruleName, ...params] = rule.split(':')
+        switch (ruleName) {
+          case 'required':
+            if (!req.body[fieldName]) {
+              errors.push({
+                [fieldName]: `${fieldName} field is missing`
+              })
+            }
+            break;
+          case 'min':
+            if (req.body[fieldName]) {
+              const min = parseInt(params[0])
+              if (req.body[fieldName].length < min) {
+                errors.push({
+                  [fieldName]: `${fieldName} is too short. Min value is ${min}`
+                })
+              } 
+            }
+            break;
+          case 'max':
+            if (req.body[fieldName]) {
+              const max = parseInt(params[0])
+              if (req.body[fieldName].length > max) {
+                errors.push({
+                  [fieldName]: `${fieldName} is too long. Max value is ${max}`
+                })
+              } 
+            }
+            break;
+          case 'email':
+            const regExpEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if (!regExpEmail.test(req.body[fieldName])) {
+              errors.push({ 
+                [fieldName]: `${fieldName} is not valid email`
+              });
+            }
+            break;
+          default:
+        }
+      })
+    })
+    if (!errors.length) {
+      return next();
+    }
+    return res.status(422).send(errors)
+  }
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const authController = require("../controllers/authController");
+const authController = require('../controllers/authController');
 
-router.post("/login", authController.login);
-router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/register',
+validator({
+  email: ['required', 'email'],
+  password: ['required', 'min:5', 'max:10'],
+}), authController.register);
 
 module.exports = router;
