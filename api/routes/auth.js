9f89b28968b358faf44818fbbff954a565f72913
mishaqwerly <mishaqwerlt@gamil.com>
2021-03-10 12:56:58 +0200

    add unique validator

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,11 +3,17 @@ const validator = require('../middleware/validator');
 var router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/login', authController.login);
+router.post('/login',
+  validator({
+    email: ['required', 'email'],
+    password: ['required', 'min:5', 'max:10'],
+  }), 
+  authController.login);
 router.post('/register',
-validator({
-  email: ['required', 'email'],
-  password: ['required', 'min:5', 'max:10'],
-}), authController.register);
+  validator({
+    email: ['required', 'email', 'unique:users:email'],
+    password: ['required', 'min:5', 'max:10'],
+  }), 
+  authController.register);
 
 module.exports = router;
