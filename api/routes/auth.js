7ca4801fb4dfbbc6827024c49efced3bf8edb1d6
mishaqwerly <mishaqwerlt@gamil.com>
2021-03-10 12:05:11 +0200

    fix validators

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const validator = require('../middleware/validator');
 var router = express.Router();
 const authController = require('../controllers/authController');
 
